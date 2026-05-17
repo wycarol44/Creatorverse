a1bdb7c70e839c8e14a9b843e2e690d46397a569
wycarol44 <wycarol44@gmail.com>
2026-05-17 17:50:47 -0400

    update dev .env with policy Allow all access

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'insert your Project URL here'
-const API_KEY = 'insert your Project API key here'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(URL, API_KEY)
+export const supabase = createClient(supabaseUrl, supabaseKey)
